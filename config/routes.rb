dd9f4105e5d11a40b2b3df654559faef07b52932
Ignacio Pèrez Gamboa <i.perezgamboa@gmail.com>
2021-01-05 21:27:42 -0300

    1

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  "users/login"
+  get  "pages/about_us"
+  get  "pages/test"
+  
+  get "pages/index"
+  root to: "pages#index"  
+end
